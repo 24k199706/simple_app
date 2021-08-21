19bf7d49781782d88c667ad0342309190954befd
24k199706 <24k0626ryo@gmail.com>
2021-08-21 15:48:44 +0900

    simple

---
+++ db/migrate/20210821064159_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :content
+      t.integer :user_id
+      t.integer :tag_id
+      t.boolean :delete_flg
+
+      t.timestamps
+    end
+  end
+end
