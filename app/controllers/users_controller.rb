ca60e3a88f0cf1f740e8bafcb39298553e5ce30b
24k199706 <24k0626ryo@gmail.com>
2021-08-27 22:39:48 +0900

    life

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,12 @@ class UsersController < ApplicationController
     @user=User.new
   end
   def creat
-    @user=User.new(params[:user])
+    @user=User.new(user_params)
     if User.save
-      
+      flash[:success] = "Welcome to the Simple_App!"
+      redirect_to @user
     else
-      render "new"  
+      render "new"
     end
   end
   def index
@@ -18,5 +19,12 @@ class UsersController < ApplicationController
   def edit
   end
 
-  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'simples#home',as:"simple"#ホーム画面
-  get 'users/index', to:"users#index"
-  get 'users/show', to:"users#show"
-  get 'users/edit', to:"users#index"
   get 'users/new', to:"users#new", as:"new" #新規登録画面
-  post "users", to:"users#creat",as:"creat"
+  post "/users", to:"users#creat",as:"creat"
+  get 'users/index', to:"users#index" ,as:"index"
+  get 'users/id', to:"users#show",as:"user"
+  get 'users/edit', to:"users#edit",as:"edit"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
